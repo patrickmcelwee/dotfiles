e059d328da3088b22190d13dcf7e2640efdadde6
Patrick McElwee <pmm21@duke.edu>
2014-05-02 14:53:46 -0400

    Do not backup symlinks and add README
    This prevents actual files from being overwritten the second time that
    dotfiles are installed.

---
+++ README.markdown
@@ -0,0 +1,9 @@
+This currently has my bash, tmux and some script files. Need to add: vim config file.
+
+To install:
+
+    git clone https://github.com/patrickmcelwee/.files ~/.files
+    cd ~/.files
+    ./install_dotfiles.sh
+
+This will make a backup of any config files that are not symlinks. They will be placed into ~/.files\_old
--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -13,8 +13,10 @@ cd $dir
 echo "...done"
 
 for file in $files; do
-  echo "Moving existing dotfiles from ~ to $olddir"
-  mv ~/.$file $olddir
+  if [ -f ~/.$file ] && [ ! -h ~/.$file ]; then
+    echo "Moving ~/.$file from ~ to $olddir"
+    mv ~/.$file $olddir
+  fi
   echo "Creating symlink to $file in home directory."
   ln -s $dir/$file ~/.$file
 done
