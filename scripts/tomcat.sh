9592821fda64ecf4e6c2cebd4fb28af4b6ed7221
Patrick McElwee <pmm21@duke.edu>
2014-05-05 15:37:20 -0400

    Dirty fix to different tomcat version problem

--- scripts/tomcat.sh
+++ scripts/tomcat.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-export CATALINA_HOME=/usr/local/Cellar/tomcat/6.0.26/libexec
+if [ -d '/usr/local/Cellar/tomcat/6.0.26' ]; then
+  export CATALINA_HOME=/usr/local/Cellar/tomcat/6.0.26/libexec
+elif [ -d '/usr/local/Cellar/tomcat/7.0.53' ]; then
+  export CATALINA_HOME=/usr/local/Cellar/tomcat/7.0.53/libexec
+else
+  echo "ERROR: Could not locate tomcat in /usr/local/Cellar/"
+  echo "--> Script currently only supports 6.0.26 and 7.0.53"
+fi
+
 case "$1" in
   "start" )
     export CATALINA_OPTS="-Xms1024m -Xmx1024m -XX:MaxPermSize=512m -Dsolr.solr.home=/srv/solr"
